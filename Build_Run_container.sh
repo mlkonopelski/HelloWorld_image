6a0bdb034abe8f19d0cbe3fb137a7fa46e6e2e02
mlkonopelski <mlkonopelski@gmail.com>
2020-03-20 15:34:31 +0100

    Create shell command to run entire project

---
+++ Build_Run_container.sh
@@ -0,0 +1,11 @@
+docker pull https://github.com/mlkonopelski/HelloWorld_image.git  master
+
+cd ..
+docker build HelloWorld_image -t helloworld_image
+
+docker run\
+-d \
+--name helloworld_container\
+-p 8000:8080 \
+-v /home/mateusz-ubuntu/Documents/Projects/LocalData/HelloWorld_data:/app/DATA \
+helloworld_image

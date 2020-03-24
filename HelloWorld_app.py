fbbbc10cbacc5ed2d114305e72a516f8f286344c
mlkonopelski <mlkonopelski@gmail.com>
2020-03-24 13:28:12 +0100

    change app to simple version

--- HelloWorld_app.py
+++ HelloWorld_app.py
@@ -1,20 +1,7 @@
-from flask import Flask, render_template
-import pandas as pd
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def main():
-    return render_template('HelloWorld_site.html')
-
-
-def create_empty_csv():
-    pd.DataFrame(columns=['col1', 'col2']).to_csv('DATA/Test.csv', index=False)
-
-
-if __name__ == "__main__":
-    print('Here will be instructions')
-
-#   create_empty_csv()
-    app.run(host="0.0.0.0", port="8080")
+@app.route('/')
+def index():
+    return '<h1> Hello World in Heroku! </h1>'

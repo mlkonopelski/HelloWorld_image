a7f4f880f8564ddfba2d3ddd8ce436af3d39cd1b
mlkonopelski <mlkonopelski@gmail.com>
2020-03-24 12:10:09 +0100

    remove export pandas file to CSV

--- HelloWorld_app.py
+++ HelloWorld_app.py
@@ -3,16 +3,18 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 def main():
     return render_template('HelloWorld_site.html')
+
 
 def create_empty_csv():
     pd.DataFrame(columns=['col1', 'col2']).to_csv('DATA/Test.csv', index=False)
 
+
 if __name__ == "__main__":
     print('Here will be instructions')
 
-    create_empty_csv()
+#   create_empty_csv()
     app.run(host="0.0.0.0", port="8080")
